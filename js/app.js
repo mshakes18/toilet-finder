34351e519cd4c0927df50172c1d887fd38edeb05
Marcus S <marcusshakes@gmail.com>
2022-03-20 23:33:22 +0000

    card change

--- js/app.js
+++ js/app.js
@@ -3715,20 +3715,20 @@ function buildLocationList(toilets) {
   }
 }
 
-function flyToStore(currentFeature) {
+function flyToStore(toilet) {
   map.flyTo({
-    center: currentFeature.geometry.coordinates,
+    center: toilet.geometry.coordinates,
     zoom: 15
   });
 }
 
-function createPopUp(currentFeature) {
+function createPopUp(toilet) {
   const popUps = document.getElementsByClassName('mapboxgl-popup');
   /** Check if there is already a popup on the map and if so, remove it */
   if (popUps[0]) popUps[0].remove();
 
   const popup = new mapboxgl.Popup({ closeOnClick: false })
-    .setLngLat(currentFeature.geometry.coordinates)
-    .setHTML(`<h3>Sweetgreen</h3><h4>${currentFeature.properties.address}</h4>`)
+    .setLngLat(toilet.geometry.coordinates)
+    .setHTML(`<h3>Toilet</h3><h4>${toilet.properties.street_address}</h4><p>gender neutral: ${toilet.properties.gender_neutral}</p>`)
     .addTo(map);
 }
